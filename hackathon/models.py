5f003351332bb2e73317b4ed118788b4f8ab1ab5
RahulAakunuru <RahulAakunuru>
2017-08-19 16:40:52 -0700

    Added Meal Model

--- hackathon/models.py
+++ hackathon/models.py
@@ -3,9 +3,12 @@ from django.db import models
 class Product(models.Model):
     food_id         =   models.AutoField(primary_key=True)
     food_name       =   models.CharField(max_length=128, null=False, blank=False)
-    expiration_date =   models.DateField(auto_now_add=False,auto_created=False)
+    expiration_date =   models.IntegerField(null=False, blank=False)
     location        =   models.CharField(max_length=128)
     Image           =   models.CharField(max_length=100, null=True, blank=True)
 
-#class Meal(models.Model):
-#
+class Meal(models.Model):
+    food_id     = models.ForeignKey('Product',on_delete=models.CASCADE)
+    meal_id     = models.CharField(max_length=128, null= False, blank= False)
+    mean_name   = models.CharField(max_length=128, null=False, blank=False)
+    quantity    = models.IntegerField(null=False, blank=False)
