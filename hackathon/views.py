5fb1667ca0974c0235bd9b338f235850d9ff1f19
RahulAakunuru <RahulAakunuru>
2017-08-20 11:08:41 -0700

    Change in CSS

--- hackathon/views.py
+++ hackathon/views.py
@@ -44,22 +44,16 @@ class MyPantrySearch(View):
 class MyPantryAddProduct(View):
     def get(self, request, *args, **kwargs):
         food_name = request.GET['food_name'].strip()
-        print("##############Just before get Expiration###############")
         food_expiry_date = request.GET['food_expiration_date'].strip()
-        print("##############Just after get Expiration###############")
         food_id = Product.objects.all().count() + 1
         location = 'Fridge'
         product = Product()
         product.food_id = str(food_id)
         product.food_name = food_name
-        print("##############Just before Expiration###############")
         print(type(food_expiry_date))
         product.expiration_date = parser.parse(food_expiry_date)
-        print("##############Just after Expiration###############")
         product.location = location
-        print("##############Just Before Save###############")
         product.save()
-        print("##############Just After Save###############")
         querySet = Product.objects.all()
         temp = []
         object_list = []
