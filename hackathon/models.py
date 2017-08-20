b51fe43fc4a2035f145bd7fbd5b1a195efb3218b
RahulAakunuru <RahulAakunuru>
2017-08-19 21:59:05 -0700

    Updated Model. Changed Integer to CharField

--- hackathon/models.py
+++ hackathon/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     food_id         =   models.AutoField(primary_key=True)
     food_name       =   models.CharField(max_length=128, null=False, blank=False)
-    expiration_date =   models.IntegerField(null=False, blank=False)
+    expiration_date =   models.DateField(auto_now=False, auto_now_add=False)
     location        =   models.CharField(max_length=128)
     Image           =   models.CharField(max_length=100, null=True, blank=True)
 
@@ -11,4 +11,4 @@ class Meal(models.Model):
     food_id     = models.ForeignKey('Product',on_delete=models.CASCADE)
     meal_id     = models.CharField(max_length=128, null= False, blank= False)
     mean_name   = models.CharField(max_length=128, null=False, blank=False)
-    quantity    = models.IntegerField(null=False, blank=False)
+    quantity    = models.CharField(max_length=3, null=False, blank=False)
