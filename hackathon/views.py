5186b6721e5e728f7cfe004f16c0795276feb9dc
RahulAakunuru <RahulAakunuru>
2017-08-19 20:20:42 -0700

    Initial Home Page

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import View
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        querySet = Product.objects.all()
+        context = {"object_list" : querySet}
+        return render(request, 'home.html', context)
